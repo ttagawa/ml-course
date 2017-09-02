68e43d087211d647f95565d82e90edd387d6a1f8
ttagawa <tylertagawa@gmail.com>
2017-09-01 22:14:05 -0700

    added normal equation implentation.

--- ex1/normalEqn.m
+++ ex1/normalEqn.m
@@ -1,9 +1,9 @@
 function [theta] = normalEqn(X, y)
-%NORMALEQN Computes the closed-form solution to linear regression 
-%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%NORMALEQN Computes the closed-form solution to linear regression
+%   NORMALEQN(X,y) computes the closed-form solution to linear
 %   regression using the normal equations.
 
-theta = zeros(size(X, 2), 1);
+theta = pinv(X'*X)*X'*y;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the code to compute the closed form solution
